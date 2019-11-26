88ba7850155dd3e7bc74b6910a418b019e5ddd32
yhc <445727994@qq.com>
2019-11-27 01:04:00 +0800

    about

---
+++ extend/lang/admin/order.php
@@ -0,0 +1,22 @@
+<?php 
+ $LANG= [
+'index'=>'订单列表',
+'add'=>'添加订单',
+'edit'=>'编辑订单',
+'search_array'=>[
+'id'=>'输入id查找',
+],
+'field'=>[
+'order_no'=>'订单号',
+'user_id'=>'购买人',
+'money'=>'金额',
+'goods_id'=>'预约商品',
+'goods_content'=>'预约时商品信息',
+'form_content'=>'自定义字段',
+'status'=>'状态',
+'create_time'=>'创建时间',
+'update_time'=>'更新时间',
+],
+
+]; 
+return $LANG;
